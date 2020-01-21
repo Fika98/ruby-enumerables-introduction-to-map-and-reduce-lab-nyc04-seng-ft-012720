ebe4de2e985772b669d8a2556a5b43d8899b25ec
Firuz Afzalov <afzalov98@gmail.com>
2020-01-21 00:35:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,8 +52,8 @@ end
 def reduce_to_all_true(source_array)
   i = 0 
   while i < source_array.length do 
-     if !source_array[i]
-       return false
+     return false if !source_array[i]
+      
     i += 1 
   end 
    return true
