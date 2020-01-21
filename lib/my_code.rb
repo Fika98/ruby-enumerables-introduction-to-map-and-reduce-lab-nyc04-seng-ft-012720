b227688d83796c19151a63cf26f74a35eeac4e4a
Firuz Afzalov <afzalov98@gmail.com>
2020-01-21 00:28:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,7 +38,7 @@ end
 return new 
 end 
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = 0)
   total = starting_point
   i = 0 
   while i < source_array.length do 
